613c23e3aef6c6eee635d61ca14c8ba0e5ef677d
dbseend <shyoon840@gmail.com>
2023-09-02 16:43:45 +0900

    "종료하기" 문구 추가

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -31,6 +31,7 @@ public class WordManager {
             int menu = selectMenu();
 
             if (menu == 0) {
+                System.out.println("프로그램 종료! 다음에 만나요~");
                 break;
             }
             if (menu == 4) {
